881bac00ec7f3a77523509b8a4b42a6321f35d1f
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-04 15:32:26 +0300

    creating the app

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter as router, Switch, Route } from "react-router-dom";
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
