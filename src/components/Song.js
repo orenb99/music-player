d2ba0874dd3b7bac3ef7b20fc291eff3ea2e1edb
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-06 19:22:26 +0300

    added img

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import songs from "../data/songs.json";
-import imgArray from "../data/imgs/imgArray";
+import albums from "../data/albums.json";
+import { albumImgs } from "../data/imgs/imgArray";
+function getImage(item) {
+  let copy = { ...item };
+  copy = albums.find((value) => value.name === copy.album);
+  return albumImgs.find((value) => value.name === copy.cover).img;
+}
 const Song = ({ match }) => {
-  const [song, setSong] = useState(
-    songs.find((value) => value.ID === match.params.id)
-  );
+  const song = songs.find((value) => value.ID === match.params.id);
   const newLyrics = song.lyrics.split("\n").map((str, index) => (
     <p>
       {str}
@@ -15,8 +19,8 @@ const Song = ({ match }) => {
   return (
     <div className="song-page">
       <h1>{song.name}</h1>
+      <img src={getImage(song)} />
       <h1>{song.artist}</h1>
-      <h1>{song.album}</h1>
       <iframe
         width="560"
         height="315"
