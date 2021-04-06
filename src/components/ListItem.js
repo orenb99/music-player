2fec625adf69c2e96860da6c81246bfd0765cd44
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-06 13:13:46 +0300

    making the items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 
 const ListItem = ({ type, item, getImage }) => {
   return (
-    <Link
-      to={`/${type.slice(0, type.length - 1)}/${item.name}`}
-      key={item.name}
-    >
-      <img
-        className={`${type}-cover`}
-        src={getImage(type, item)}
-        alt={item.name}
-      />
-    </Link>
+    <div>
+      <Link
+        to={`/${type.slice(0, type.length - 1)}/${item.name}`}
+        key={item.name}
+      >
+        <img
+          className={`${type}-cover`}
+          src={getImage(type, item)}
+          alt={item.name}
+        />
+        <h3>{item.name}</h3>
+        {type === "songs" && <h4>{item.album}</h4>}
+        {(type === "albums" || type === "songs") && <h4>{item.artist}</h4>}
+      </Link>
+    </div>
   );
 };
 
